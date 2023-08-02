4ba97a7035b2d50a810cd3c128511a0b22e1aedb
alwanfauzy <alwanfauzi13@gmail.com>
2023-08-03 06:55:13 +0700

    remove easyloading

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:json_theme/json_theme.dart';
-import 'package:story_ku/data/pref/token_pref.dart';
 import 'package:story_ku/routes/router_delegate.dart';
 
 void main() async {
@@ -33,22 +31,6 @@ Future<ThemeData?> initializeTheme() async {
   return theme;
 }
 
-void configLoading() {
-  EasyLoading.instance
-    ..displayDuration = const Duration(milliseconds: 2000)
-    ..indicatorType = EasyLoadingIndicatorType.fadingCircle
-    ..loadingStyle = EasyLoadingStyle.dark
-    ..indicatorSize = 45.0
-    ..radius = 10.0
-    ..progressColor = Colors.yellow
-    ..backgroundColor = Colors.green
-    ..indicatorColor = Colors.yellow
-    ..textColor = Colors.yellow
-    ..maskColor = Colors.blue.withOpacity(0.5)
-    ..userInteractions = false
-    ..dismissOnTap = false;
-}
-
 class MyApp extends StatefulWidget {
   final ThemeData? theme;
 
@@ -73,12 +55,10 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       title: 'StoryKu',
       theme: widget.theme,
-      // home: (widget.token.isEmpty) ? const LoginPage() : const ListStoryPage(),
       home: Router(
         routerDelegate: _myRouterDelegate,
         backButtonDispatcher: RootBackButtonDispatcher(),
       ),
-      builder: EasyLoading.init(),
     );
   }
 }
